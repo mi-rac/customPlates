73e3fad5de28ebfb64a3d34cacfdeb7758145d99
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-02 14:03:28 +0100

    Added Plates entity

---
+++ src/main/java/plates/backend/Plates.java
@@ -0,0 +1,51 @@
+package plates.backend;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Plates implements java.io.Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int plateid;
+    private String registrationNumber;
+    private boolean sold;
+    private double price;
+
+    public int getPlateid() {
+        return plateid;
+    }
+
+    public void setPlateid(int plateid) {
+        this.plateid = plateid;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public boolean isSold() {
+        return sold;
+    }
+
+    public void setSold(boolean sold) {
+        this.sold = sold;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
