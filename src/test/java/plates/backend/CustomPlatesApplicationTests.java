b2344687ade109b3fbfabc5aabb93625ae28bef3
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-02 14:18:02 +0100

    Added first contextLoads() test

--- src/test/java/plates/backend/CustomPlatesApplicationTests.java
+++ src/test/java/plates/backend/CustomPlatesApplicationTests.java
@@ -1,14 +1,18 @@
 package plates.backend;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomPlatesApplicationTests
 {
-
+    @Autowired
+    private PlatesRepository platesController;
     @Test
     void contextLoads() {
+        assertThat(platesController).isNotNull();
     }
 
 }
