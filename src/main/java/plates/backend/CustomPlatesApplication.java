ca5bc32d8be9861d39c3d2cc46736c7dea91ecdb
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-02 13:20:14 +0100

    Added application.properties

--- src/main/java/plates/backend/CustomPlatesApplication.java
+++ src/main/java/plates/backend/CustomPlatesApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomPlatesApplication
 {
-
     public static void main(String[] args) {
         SpringApplication.run(CustomPlatesApplication.class, args);
     }
