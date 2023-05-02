c6bf72b4bbb6ab67dec19edd8f93265ebb4dc047
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-02 11:56:03 +0100

    First Commit - loaded dependencies

---
+++ src/main/java/plates/backend/CustomPlatesApplication.java
@@ -0,0 +1,14 @@
+package plates.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPlatesApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomPlatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/plates/backend/CustomPlatesApplicationTests.java
@@ -0,0 +1,14 @@
+package plates.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomPlatesApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
