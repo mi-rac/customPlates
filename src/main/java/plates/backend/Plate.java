367146b81ffa4da90410389e42f757f46b2fc4aa
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-04 20:58:15 +0100

    - switched back to SpringBoot 2.4

--- src/main/java/plates/backend/Plate.java
+++ src/main/java/plates/backend/Plate.java
@@ -1,10 +1,11 @@
 package plates.backend;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
