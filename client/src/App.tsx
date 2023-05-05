17ad508d5d6ff0942697f86a1ac3011163cd19b7
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-05 21:53:03 +0100

    fixed the remaining URL's

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import { Plate } from './api-generated';
 
 function App() {
     const [plates, setPlates] = useState<Plate[]>([]);
-    const apiPlates = new PlateEntityApi("server:8080");
+    const apiPlates = new PlateEntityApi("http://localhost:8080");
 
     const handleSearch = (searchTerm: string) => {
         console.log("Searching for: ", searchTerm);
